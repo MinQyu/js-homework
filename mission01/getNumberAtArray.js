e67d122bc5c55b3e4c6f81905963526847f936ba
MinQyu <jorushs@gmail.com>
2024-11-03 10:44:31 +0900

    mission01

---
+++ mission01/getNumberAtArray.js
@@ -0,0 +1,11 @@
+function getNumberAtArray(arr, index) {
+  const number = arr[index];
+  return number !== undefined ? number : "Error!";
+}
+
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(getNumberAtArray(numbers, 2)); // 30
+console.log(getNumberAtArray(numbers, 4)); // 50
+console.log(getNumberAtArray(numbers, 5)); // Error!
+console.log(getNumberAtArray(numbers, -1)); // Error!
