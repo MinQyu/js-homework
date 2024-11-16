30b872aa33dafd6b0f9615f5414edc313fa12f6a
MinQyu <jorushs@gmail.com>
2024-11-17 08:04:33 +0900

    main.js 완성

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,52 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+const loginForm = document.querySelector(".login-form");
 
+function handleSubmit(e) {
+  e.preventDefault();
+  const idInput = loginForm.querySelector("#userEmail");
+  const idInputValue = idInput.value;
+  const pwInput = loginForm.querySelector("#userPassword");
+  const pwInputValue = pwInput.value;
 
+  if (!emailReg(idInputValue)) {
+    idInput.classList.add("is--invalid");
+    return;
+  } else {
+    idInput.classList.remove("is--invalid");
+  }
 
+  if (!pwReg(pwInputValue)) {
+    pwInput.classList.add("is--invalid");
+    return;
+  } else {
+    pwInput.classList.remove("is--invalid");
+  }
 
+  if (!(idInputValue === user.id)) {
+    alert("id가 일치하지 않습니다");
+    return;
+  } else {
+    if (!(pwInputValue === user.pw)) {
+      alert("비밀번호가 일치하지 않습니다");
+      return;
+    } else {
+      window.location.href = "welcome.html";
+    }
+  }
+}
 
-
-
-
-
-
+loginForm.addEventListener("submit", handleSubmit);
