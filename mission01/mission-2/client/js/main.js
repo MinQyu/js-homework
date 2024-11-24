0ada7c08a55d744770ab284f2b760b6ddd9df665
Min-gyu <jorushs@gmail.com>
2024-11-24 13:02:19 +0900

    미션2 완성

--- mission01/mission-2/client/js/main.js
+++ mission01/mission-2/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,40 @@
 
 */
 
+const navItems = document.querySelectorAll(".nav li");
+const selectedImage = document.querySelector(".visual img");
+const nickName = document.querySelector(".nickName");
 
+function setBgColor(index) {
+  const [colorA, colorB] = data[index].color;
+  document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
+}
 
+function setImage(index) {
+  const selectedImageSrc = `./assets/${data[index].name}.jpeg`;
+  const selectedImageAlt = data[index].alt;
 
+  selectedImage.setAttribute("src", selectedImageSrc);
+  selectedImage.setAttribute("alt", selectedImageAlt);
+}
 
+function setNameText(index) {
+  const selecteImageName = data[index].name;
+  nickName.textContent = selecteImageName;
+}
 
+function handleClick(e) {
+  const target = e.currentTarget;
+  const index = parseInt(target.dataset.index) - 1;
 
+  document.querySelector(".nav .is-active").classList.remove("is-active");
+  target.classList.add("is-active");
 
+  setBgColor(index);
+  setImage(index);
+  setNameText(index);
+}
 
-
-
-
-
-
+navItems.forEach((item) => {
+  item.addEventListener("click", handleClick);
+});
